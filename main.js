a05e0976f7a44d6dbab98af78dc5c74ce4ddabe4
makary87 <makary1987@gmail.com>
2023-07-22 12:18:50 +0200

    modul_3_lekcja_9

---
+++ main.js
@@ -0,0 +1,8 @@
+alert('WTF z main.js');
+// console.log(blad);
+console.log('Log z main.js');
+
+const birthYear = 1987
+const firstName = 'Michal';
+console.log('Imie to: ' + firstName + ' born in ' + birthYear);
+console.log(`Imie to: ${firstName} born in ${birthYear}`);
