1ad69dbd271f4518658378d936c9d0d68959928e
makary87 <makary1987@gmail.com>
2023-08-08 21:13:16 +0200

    modul_4_lekcja_11

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert('WTF z main.js');
+// alert('WTF z main.js');
 // console.log(blad);
 console.log('Log z main.js');
 
@@ -6,3 +6,11 @@ const birthYear = 1987
 const firstName = 'Michal';
 console.log('Imie to: ' + firstName + ' born in ' + birthYear);
 console.log(`Imie to: ${firstName} born in ${birthYear}`);
+
+// Modul 4 lekcja 11
+const header = document.querySelector('.page-header__heading--js');
+header.innerHTML = 'Nowa tresc'
+console.log(header)
+
+console.log(header.style)
+header.style.color = 'blue'; // do modyfikacji CSS za pomoca JS
