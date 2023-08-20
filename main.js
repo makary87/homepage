6995145715a5ead5542c0afb24d7f34a1ec35182
makary87 <makary1987@gmail.com>
2023-08-20 12:19:09 +0200

    modul_6_lekcja_13

--- main.js
+++ main.js
@@ -68,3 +68,23 @@ const clickFunction = (e) => {
     header.innerHTML = 'Nowa wartość nagłówka';
 }
 button.addEventListener('click', clickFunction);
+
+// Modul 6 lekcja 13 (hamburger menu)
+button.classList.add('testowa')
+console.log(button.classList)
+console.log(button.classList.contains('testowa'))
+
+button.classList.toggle('testowa')
+console.log(button.classList)
+console.log(button.classList.contains('testowa'))
+
+button.classList.toggle('testowa')
+console.log(button.classList)
+console.log(button.classList.contains('testowa'))
+
+const hamburger = document.querySelector('.modul-6-lekcja-13__hamburger--js')
+console.log(hamburger)
+hamburger.addEventListener('click', () => {
+    const navigation = document.querySelector('.modul-6-lekcja-13__navigation--js')
+    navigation.classList.toggle('modul-6-lekcja-13__navigation--open')
+})
